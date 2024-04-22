37b88685ca85dae4b3e286bbb3889c0d895e128c
Maribel <maribel.lorenzo.delafuente@gmail.com>
2024-04-22 21:20:05 +0200

    create user-mnodel

---
+++ models/plan.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const { REQUIRED_FIELD_ERROR } = require("../constants/errorMessages");
+const cuisineTypes = require("../constants/cuisineTypes");
+
+const userSchema = new mongoose.Schema({
+  planname: {
+    type: String,
+    required: [true, REQUIRED_FIELD_ERROR],
+    trim: true,
+  },
+  date: {
+    type: String,
+    required: [true, REQUIRED_FIELD_ERROR],
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: [true, REQUIRED_FIELD_ERROR],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, REQUIRED_FIELD_ERROR],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, REQUIRED_FIELD_ERROR],
+  },
+  cuisineType: {
+    type: [String],
+    required: [true, REQUIRED_FIELD_ERROR],
+    enum: cuisineTypes, //en la carpeta de constantes están enumerados todos los tipos
+  },
+  image: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+});
