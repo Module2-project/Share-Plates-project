bdf5a8e330114ddcf65e4348c61a4628fa91f8e2
Maribel <maribel.lorenzo.delafuente@gmail.com>
2024-04-24 21:03:28 +0200

    PlanModel

--- constants/cuisineTypes.js
+++ constants/cuisineTypes.js
@@ -1,1 +1,1 @@
-module.exports = ['Italiana', 'Mexicana', 'Asiática', 'Americana', 'Mediterránea', 'Francesa', 'Española', 'India', 'Tailandesa', 'Griega', 'Árabe', 'Peruana', 'Brasileña', 'Argentina', 'Fusión']
+module.exports = ['Italiana', 'Mexicana', 'Asiática', 'Americana', 'Mediterránea', 'Francesa', 'Española', 'India', 'Tailandesa', 'Griega', 'Árabe', 'Peruana', 'Brasileña', 'Argentina', 'Fusión', 'Vegana', 'Saludable']
